448b376368cde3277571b94eac02e0a42f58c002
Dustin Watts <dustin_watts@yahoo.com>
2020-08-29 17:52:09 +0200

    Various Updates

--- FreeTouchDeck.ino
+++ FreeTouchDeck.ino
@@ -15,7 +15,7 @@
   then necessary. It also lacks good documentation and comments in the code.
 
   tft_config.h holds the configuration for the TFT display and the touch controller. This is where
-  SPI pins are defined. Uncomment the appropriate lines.
+  SPI pins are defined. Uncomment the lines needed for your display.
 
   The SPIFFS (FLASH filing system) is used to hold touch screen calibration data.
   It has to be runs at least once. After that you can set REPEAT_CAL to false.
@@ -39,6 +39,24 @@
 #include <TFT_eSPI.h>
 #include <BleKeyboard.h>
 #include <ArduinoJson.h>
+
+#include "BLEDevice.h"
+#include "BLEUtils.h"
+#include "BLEBeacon.h"
+#include "esp_sleep.h"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* host = "FreeTouchDeck";
+WebServer server(80);
+
+//holds the current upload
+File fsUploadFile;
 
 BleKeyboard bleKeyboard("FreeTouchDeck", "Made by me");
 
@@ -98,9 +116,42 @@ void setup() {
   
   // Use serial port
   Serial.begin(9600);
+  
+//------------------WIFI Initialization ------------------------------------------------------------------------
 
-  bleKeyboard.begin();
+  Serial.printf("Connecting to %s\n", ssid);
+  if (String(WiFi.SSID()) != String(ssid)) {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+  }
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  MDNS.begin(host);
+
+//------------------Webserver Initialization ------------------------------------------------------------------------
+
+  // Restart handle
+  server.on("/restart", HTTP_POST, handleRestart);
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri())) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+
+  server.on("/saveconfig.htm", HTTP_POST, saveconfig);
+
+//------------------TFT/Touch Initialization ------------------------------------------------------------------------
+    
   // Initialise the TFT screen
   tft.init();
 
@@ -121,18 +172,28 @@ void setup() {
 
   // Setup the Font used for plain text
   tft.setFreeFont(LABEL_FONT);
-  
-  //readButtonConfig();
 
   // Draw keypad
   drawKeypad();
+  
+//------------------BLE Initialization ------------------------------------------------------------------------
+
+  bleKeyboard.begin();
 
 }
 
 //------------------------------------------------------------------------------------------
 
 void loop(void) {
-  // To store the touch coordinates
+  
+  if(pageNum == 7){
+
+  // If the pageNum is set to 7, do no draw anything on screen or check for touch
+  server.handleClient();
+
+  }else{
+  
+  // Touch coordinates are stored here
   uint16_t t_x = 0, t_y = 0; 
 
   // Pressed will be set true is there is a valid touch on the screen
@@ -152,14 +213,14 @@ void loop(void) {
 
     if (key[b].justReleased()) {
 
-        // draw normal button space (non inverted)
+        // Draw normal button space (non inverted)
         key[b].drawButton();     
         // Call the drawLogo function with generalconfig.logoColour foreground (normal state)
         drawlogo(b,colArray[b],rowArray[b],generalconfig.logoColour);
     }
 
     if (key[b].justPressed()) {
-        key[b].drawButton(true);  // draw inverted button space (white)
+        key[b].drawButton(true);  // Draw inverted button space (white)
         if(b == 5 | pageNum == 0){
         // Call the drawLogo function with menuButtonColour foreground (inverse state)
         drawlogo(b,colArray[b],rowArray[b],generalconfig.menuButtonColour);
@@ -173,74 +234,81 @@ void loop(void) {
       if (b == 0) {
           //Button 0 functions
         if(pageNum == 0){
-          //Page 0 button function
+          //Page 0 button 0 function
           pageNum = 1;    // By setting pageNum to 1
           drawKeypad();   // and calling drawKeypad() a new keypad is drawn with pageNum 1
         }else if(pageNum == 1){
-          //Page 1 button function
+          //Page 1 button 0 function
           bleKeyboard.write(KEY_MEDIA_MUTE);
         }else if(pageNum == 2){
-          //Page 2 button function
+          //Page 2 button 0 function
           bleKeyboard.write(KEY_MEDIA_MUTE); 
         }else if(pageNum == 3){
-          //Page 3 button function
+          //Page 3 button 0 function
           bleKeyboard.press(KEY_LEFT_CTRL);
           bleKeyboard.print("t");
           delay(50);
           bleKeyboard.releaseAll();
         }else if(pageNum == 4){
-          //Page 4 button function
+          //Page 4 button 0 function
         }else if(pageNum == 5){
-          //Page 5 button function
+          //Page 5 button 0 function
+        }else if(pageNum == 6){
+          // Enter config mode
+          pageNum = 7;    // By setting pageNum to 7
+          configmode();
+          drawKeypad();   // and calling drawKeypad() a new keypad is drawn with pageNum 7
         }
       }
       
       if (b == 1) {
         //Button 1 functions
         if(pageNum == 0){
-          //Page 0 button function
+          //Page 0 button 1 function
           pageNum = 2;    // By setting pageNum to 2
           drawKeypad();   // and calling drawKeypad() a new keypad is drawn with pageNum 2
         }else if(pageNum == 1){
-          //Page 1 button function
+          //Page 1 button 1 function
           bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
         }else if(pageNum == 2){
-          //Page 2 button function
+          //Page 2 button 1 function
         }else if(pageNum == 3){
-          //Page 3 button function
+          //Page 3 button 1 function
           bleKeyboard.press(KEY_LEFT_CTRL);
           bleKeyboard.press(KEY_LEFT_SHIFT);
           bleKeyboard.press(KEY_TAB);
           delay(50);
           bleKeyboard.releaseAll();   
         }else if(pageNum == 4){
-          //Page 4 button function
+          //Page 4 button 1 function
         }else if(pageNum == 5){
-          //Page 5 button function
+          //Page 5 button 1 function
+        }else if(pageNum == 6){
+          //Page 6 button 1 function
         }
       }
 
       if (b == 2) {
         //Button 2 functions
         if(pageNum == 0){
-          //Page 0 button function
+          //Page 0 button 2 function
           pageNum = 3;    // By setting pageNum to 3
           drawKeypad();   // and calling drawKeypad(), a new keypad is drawn with pageNum 3    
         }else if(pageNum == 1){
-          //Page 1 button function
+          //Page 1 button 2 function
           bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
         }else if(pageNum == 2){
-          //Page 2 button function
+          //Page 2 button 2 function
         }else if(pageNum == 3){
-          //Page 3 button function
+          //Page 3 button 2 function
           bleKeyboard.press(KEY_LEFT_CTRL);
           bleKeyboard.press(KEY_TAB);
           delay(50);
           bleKeyboard.releaseAll();
         }else if(pageNum == 4){
-          //Page 4 button function
+          //Page 4 button 2 function
         }else if(pageNum == 5){
-          //Page 5 button function
+          //Page 5 button 2 function
         }
       }
 
@@ -251,47 +319,47 @@ void loop(void) {
           pageNum = 4;    // By setting pageNum to 4
           drawKeypad();   // and calling drawKeypad() a new keypad is drawn with pageNum 4
         }else if(pageNum == 1){
-          //Page 1 button function
+          //Page 1 button 3 function
           bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
         }else if(pageNum == 2){
-          //Page 2 button function
+          //Page 2 button 3 function
         }else if(pageNum == 3){
-          //Page 3 button function
+          //Page 3 button 3 function
         }else if(pageNum == 4){
-          //Page 4 button function
+          //Page 4 button 3 function
         }else if(pageNum == 5){
-          //Page 5 button function
+          //Page 5 button 3 function
         }
       }
 
       if (b == 4) {
         //Button 4 functions
         if(pageNum == 0){
-          //Page 0 button function
+          //Page 0 button 4 function
           pageNum = 5;    // By setting pageNum to 5
           drawKeypad();   // and calling drawKeypad() a new keypad is drawn with pageNum 5
         }else if(pageNum == 1){
-          //Page 1 button function
+          //Page 1 button 4 function
         }else if(pageNum == 2){
-          //Page 2 button function
+          //Page 2 button 4 function
           bleKeyboard.write(KEY_MEDIA_STOP);
         }else if(pageNum == 3){
-          //Page 3 button function
+          //Page 3 button 4 function
         }else if(pageNum == 4){
-          //Page 4 button function
+          //Page 4 button 4 function
         }else if(pageNum == 5){
-          //Page 5 button function
+          //Page 5 button 4 function
         }
       }
 
       if (b == 5) {
         //Button 5 functions
         if(pageNum == 0){
-          //Page 0 button function
+          //Page 0 button 5 function
           pageNum = 6;    // By setting pageNum to 6
           drawKeypad();   // and calling drawKeypad() a new keypad is drawn with pageNum 6
         }else{
-          pageNum = 0; // Button 5 is always the "back home button"
+          pageNum = 0; // On any other page button 5 is always the "back home button"
           drawKeypad();
         }
       }
@@ -300,6 +368,7 @@ void loop(void) {
       
     }
   }
+}
 }
 
 //----------- Drawing Buttons -------------------------------------------------------------------------------
@@ -318,13 +387,22 @@ void drawKeypad()
                           KEY_W, KEY_H, TFT_WHITE, generalconfig.menuButtonColour, 0xFFFF,
                           "", KEY_TEXTSIZE);
         key[b].drawButton();
-        drawlogo(b,col,row,generalconfig.logoColour); // After drawing the button outline we call this to draw a logo.
-      
+        drawlogo(b,col,row,generalconfig.logoColour); // After drawing the button outline we call this to draw a logo.   
       }
     }
 
-  // Draw the function button outlines and fill them with colours   
+  }else if(pageNum == 7){
+    // Config mode...
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(20, 20);
+    tft.setTextFont(2);
+    tft.setTextSize(2);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.println("Now in config mode. Go to:");
+    tft.println("  http://freetouchdeck.local");
+    tft.println("  to configure.");
   }else{
+  // Draw the function button outlines and fill them with colours   
     for (uint8_t row = 0; row < 2; row++) {
       for (uint8_t col = 0; col < 3; col++) {
         
@@ -371,7 +449,7 @@ void drawlogo(int logonumber, int col, int row, uint16_t fgcolor){
     }else if(logonumber == 4){
     tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), discord, logoWidth, logoHeight, fgcolor);
     }else if(logonumber == 5){
-    tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), youtube, logoWidth, logoHeight, fgcolor);
+    tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), settings, logoWidth, logoHeight, fgcolor);
     }
   }else if (pageNum == 1){
     // pageNum 1 contains the Music logo's
@@ -449,9 +527,9 @@ void drawlogo(int logonumber, int col, int row, uint16_t fgcolor){
     tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), home_button, logoWidth, logoHeight, fgcolor);
     }
   }else if (pageNum == 6){
-    // pageNum 6 contains the Youtube logo's
+    // pageNum 6 contains the Settings logo's
     if(logonumber == 0){
-    tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), empty_button, logoWidth, logoHeight, fgcolor);
+    tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), spanner, logoWidth, logoHeight, fgcolor);
     }else if(logonumber == 1){
     tft.drawXBitmap(KEY_X - 37 + col * (KEY_W + KEY_SPACING_X), KEY_Y - 37 + row * (KEY_H + KEY_SPACING_Y), empty_button, logoWidth, logoHeight, fgcolor);
     }else if(logonumber == 2){
@@ -470,7 +548,7 @@ void drawlogo(int logonumber, int col, int row, uint16_t fgcolor){
 
 void loadGeneralConfig(){
 
-  if(SPIFFS.exists("/generalconfig.json") == true){
+  if(SPIFFS.exists("/generalconfig.json")){
 
   File generalconfigfile = SPIFFS.open("/generalconfig.json", "r");
 
@@ -479,7 +557,7 @@ void loadGeneralConfig(){
 
   DeserializationError error = deserializeJson(doc, generalconfigfile);
 
-  // Test if parsing succeeds.
+  // If parsing failed
   if (error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.c_str());
@@ -519,25 +597,6 @@ void loadGeneralConfig(){
   
 }
 
-//----------- Load Button Config -------------------------------------------------------------------------------
-
-void readButtonConfig(){
-
-  File buttonconfig = SPIFFS.open("/buttons.json", "r");
-
-  const size_t capacity = 2*JSON_OBJECT_SIZE(1) + 11*JSON_OBJECT_SIZE(2) + 2*JSON_OBJECT_SIZE(5) + 363;
-  DynamicJsonDocument doc(capacity);
-
-  deserializeJson(doc, buttonconfig);
-
-  const char* logo = doc["homepage"]["buttons"]["0"]["logo"]; //Get the logo
-  const char* function = doc["homepage"]["buttons"]["0"]["function"]; //Get the function
-
-  homepage.logo = (const unsigned char*)logo;
-
-  buttonconfig.close();
-}
-
 //----------- Draw Error Message -------------------------------------------------------------------------------
 
 void drawErrorMessage(String message){
@@ -548,6 +607,71 @@ void drawErrorMessage(String message){
     tft.setTextSize(1);
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.println(message);
+  
+}
+
+// ------------------ WIFI Stuff ----------------------------------------
+
+void configmode() {
+
+  // Stop BLE from interfering with our WIFI signal
+  btStop();
+  esp_bt_controller_disable();
+  esp_bt_controller_deinit();
+  esp_bt_controller_mem_release(ESP_BT_MODE_IDLE);
+
+  // Set pageNum to 7 so no buttons are displayed and touches are ignored
+  pageNum = 7;
+  Serial.println("Entering Config Mode");
+
+  // Start the webserver
+  server.begin();
+  Serial.println("HTTP Server Started");
+  
+}
+
+void handleRestart(){ 
+
+  // First send some text to the browser otherwise an ugly browser error shows up
+  server.send(200, "text/plain", "FreeTouchDeck is restarting...");
+  // Then restart the ESP
+  Serial.println("ESP Restarting...");
+  ESP.restart();
+
+}
+
+void saveconfig(){
+
+  // Delete existing file, otherwise the configuration is appended to the file
+  SPIFFS.remove("/generalconfig.json");
+
+  // Open file for writing
+  File file = SPIFFS.open("/generalconfig.json", "w");
+  if (!file) {
+    Serial.println(F("Failed to create file"));
+    return;
+  }
+
+  //Create a JSON document and a JSON object
+  StaticJsonDocument<256> doc;
+  JsonObject obj1 = doc.to<JsonObject>();
+
+  // Loop trough all agruments received and append them to our object
+  for (int i = 0; i < server.args(); i++) {
+    uint16_t value = server.arg(i).toInt();
+    obj1[server.argName(i)] = value;
+  }
+
+  // Serialize JSON to file
+  if (serializeJsonPretty(doc, file) == 0) {
+    Serial.println(F("Failed to write to file"));
+  }
+
+  // Close the file
+  file.close();
+
+  // Display "Configuration saved" page
+  handleFileRead("/saveconfig.htm");
   
 }
 
@@ -616,4 +740,68 @@ void touch_calibrate()
       f.close();
     }
   }
+}
+
+//----------- Functions used by Webserver -------------------------------------------------------------------------------
+
+String getContentType(String filename) {
+  if (server.hasArg("download")) {
+    return "application/octet-stream";
+  } else if (filename.endsWith(".htm")) {
+    return "text/html";
+  } else if (filename.endsWith(".html")) {
+    return "text/html";
+  } else if (filename.endsWith(".css")) {
+    return "text/css";
+  } else if (filename.endsWith(".js")) {
+    return "application/javascript";
+  } else if (filename.endsWith(".png")) {
+    return "image/png";
+  } else if (filename.endsWith(".gif")) {
+    return "image/gif";
+  } else if (filename.endsWith(".jpg")) {
+    return "image/jpeg";
+  } else if (filename.endsWith(".ico")) {
+    return "image/x-icon";
+  } else if (filename.endsWith(".xml")) {
+    return "text/xml";
+  } else if (filename.endsWith(".pdf")) {
+    return "application/x-pdf";
+  } else if (filename.endsWith(".zip")) {
+    return "application/x-zip";
+  } else if (filename.endsWith(".gz")) {
+    return "application/x-gzip";
+  } else if (filename.endsWith(".json")) {
+    return "application/json";
+  }
+  return "text/plain";
+}
+
+bool exists(String path){
+  bool yes = false;
+  File file = SPIFFS.open(path, "r");
+  if(!file.isDirectory()){
+    yes = true;
+  }
+  file.close();
+  return yes;
+}
+
+bool handleFileRead(String path) {
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) {
+    path += "index.htm";
+  }
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (exists(pathWithGz) || exists(path)) {
+    if (exists(pathWithGz)) {
+      path += ".gz";
+    }
+    File file = SPIFFS.open(path, "r");
+    server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
 }
