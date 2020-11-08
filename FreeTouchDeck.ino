c29b1c6836a7d2f3f88d8e5ca0b9289494934ea4
Dustin Watts <dustin_watts@yahoo.com>
2020-11-08 09:38:26 +0100

    touchInterruptPin disabled by default

--- FreeTouchDeck.ino
+++ FreeTouchDeck.ino
@@ -48,7 +48,7 @@
 
 // ------- Uncomment the define below if you want to use SLEEP and wake up on touch -------
 // The pin where the IRQ from the touch screen is connected uses ESP-style GPIO_NUM_* instead of just pinnumber
-#define touchInterruptPin GPIO_NUM_27
+//#define touchInterruptPin GPIO_NUM_27
 
 // ------- Uncomment the define below if you want to use a piezo buzzer and specify the pin where the speaker is connected -------
 //#define speakerPin 26
