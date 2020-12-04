0cb52bd98ea8b8e053817e9ad57097f95eaf6a25
Dustin Watts <dustin_watts@yahoo.com>
2020-12-04 20:40:23 +0100

    Version 0.9.4
    #defines set for resistive by default.

--- FreeTouchDeck.ino
+++ FreeTouchDeck.ino
@@ -39,7 +39,7 @@
 */
 
 // ------- Uncomment the next line if you use capacitive touch -------
-#define USECAPTOUCH
+//#define USECAPTOUCH
 
 // ------- Uncomment and populate the following if your cap touch uses custom i2c pins -------
 //#define CUSTOM_TOUCH_SDA 26
@@ -54,7 +54,7 @@
 #define touchInterruptPin GPIO_NUM_27
 
 // ------- Uncomment the define below if you want to use a piezo buzzer and specify the pin where the speaker is connected -------
-#define speakerPin 26
+//#define speakerPin 26
 
 const char *versionnumber = "0.9.4";
 
