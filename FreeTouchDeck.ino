1fd133b902d30aa36aadb019e7d314b59ad4f160
Dustin Watts <dustin_watts@yahoo.com>
2021-05-26 11:21:28 +0200

    USECAPTOUCH should not be defined by default
    Commented out #define USECAPTOUCH and #define speakerPin, because this is not by default for the resistive touch version of FreeTouchDeck. Oversight in previous push.

--- FreeTouchDeck.ino
+++ FreeTouchDeck.ino
@@ -40,7 +40,7 @@
 
 // ------- Uncomment the next line if you use capacitive touch -------
 // (THE ESP32 TOUCHDOWN USES THIS!)
-#define USECAPTOUCH
+//#define USECAPTOUCH
 
 // ------- Uncomment and populate the following if your cap touch uses custom i2c pins -------
 //#define CUSTOM_TOUCH_SDA 26
@@ -55,7 +55,7 @@
 #define touchInterruptPin GPIO_NUM_27
 
 // ------- Uncomment the define below if you want to use a piezo buzzer and specify the pin where the speaker is connected -------
-#define speakerPin 26
+//#define speakerPin 26
 
 const char *versionnumber = "0.9.13";
 
