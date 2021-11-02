bfe3f983c964bd22b2acc2a44f76245a88806afd
Chris Pearson <ctpearson@gmail.com>
2021-11-02 15:37:28 -0700

    Conversion to VS Code PlatformIO and ArduinoIDE
    This is an initial conversion of the FreeTouchDeck Arduino IDE project into a project
    that can compile both in the Arduino IDE and the Visual Studio Code ID with PlatformIO.
    
    Important changes:
    - Submodules were added to the git project for automatically checking out all libraries
      needed in the PlatformIO environment. Once a clone of this project is made, the
      following command must be executed from the project root directory to clone the
      required library repositories into the appropriate directories:
                git submodule update --init
    - These libraries are only used under PlatformIO, and are not used under the Arduino IDE.
    - The FreeTouchDeck.ino file is in the root of the project directory, as the Arduino IDE
      requires it to be in a directory that's named the same as the file name.
    - The FreeTouchDeck.ino file no longer contains any code, due to PlatformIO not
      supporting .ino files. That code is now located in src/FreeTouchDeck.cpp.
    - The file partitions.csv was added. It contains the partition map used to flash the
      ESP32 device as 2MB flash and 2MB SPIFFS. It is also required by PlatformIO's final
      binary check to confirm the binary is not too large.
    - The appropriate User_Setup.h file must be copied into lib/TFT_eSPI.
    
    Work remaining:
    - The text in the FreeTouchDeck.ino file needs to be worded to tell Arduino IDE users
      where the various files are now located.
    - The platformio.ini file needs to be fleshed out to support different build environments.

---
+++ src/FreeTouchDeck.cpp
@@ -0,0 +1,218 @@
+
+/*
+  Author: Dustin Watts
+  Date: 27-08-2020
+
+  My thanks goes out to Brian Lough, Colin Hickey, and the people on my Discord server
+  for helping me a lot with the code and troubleshooting! https://discord.gg/RE3XevS
+
+  FreeTouchDeck is based on the FreeDeck idea by Koriwi. It uses the TFT_eSPI library 
+  by Bodmer for the display and touch functionality and it uses an ESP32-BLE-Keyboard fork 
+  with a few modifications. For saving and loading configuration it uses cJSON.
+
+  FreeTouchDeck uses some libraries from other sources. These must be installed for 
+  FreeTouchDeck to compile and run. 
+  
+  These are those libraries:
+
+      !----------------------------- Library Dependencies --------------------------- !
+      - Adafruit-GFX-Library (version 1.10.0 or above), available through Library Manager
+      - TFT_eSPI (version 2.2.14 or above), available through Library Manager
+      - ESP32-BLE-Keyboard (forked) (latest version) download from: https://github.com/DustinWatts/ESP32-BLE-Keyboard
+      - ESPAsyncWebserver (latest version) download from: https://github.com/me-no-dev/ESPAsyncWebServer
+      - AsyncTCP (latest version) download from: https://github.com/me-no-dev/AsyncTCP
+      - TJpg_Decoder (latest version) download from https://github.com/Bodmer/TJpg_Decoder
+
+      --- If you use Capacitive touch ---
+      - Dustin Watts FT6236 Library (version 1.0.1), https://github.com/DustinWatts/FT6236
+      
+  The FILESYSTEM (SPI FLASH filing system) is used to hold touch screen calibration data.
+  It has to be runs at least once when using resistive touch. After that you can set 
+  REPEAT_CAL to false (default).
+
+  !-- Make sure you have setup your TFT display and ESP setup correctly in TFT_eSPI/user_setup.h --!
+        
+        Select the right screen driver and the board (ESP32 is the only one tested) you are
+        using. Also make sure TOUCH_CS is defined correctly. TFT_BL is also be needed!
+
+        You can find examples of User_Setup.h in the "user_setup.h Examples" folder.
+  
+*/
+
+// ------- Uncomment the next line if you use capacitive touch -------
+// (THE ESP32 TOUCHDOWN USES THIS!)
+//#define USECAPTOUCH
+
+// ------- Uncomment and populate the following if your cap touch uses custom i2c pins -------
+//#define CUSTOM_TOUCH_SDA 26
+//#define CUSTOM_TOUCH_SCL 27
+
+// PAY ATTENTION! Even if resistive touch is not used, the TOUCH pin has to be defined!
+// It can be a random unused pin.
+// TODO: Find a way around this!
+#define ACTIONS_IN_TASKS
+#include "UserConfig.h"
+
+#ifdef ARDUINO_TWATCH_BASE
+AXP20X_Class *power = new AXP20X_Class();
+#endif
+static const char *module = "FreeTouchDeck";
+const char *versionnumber = "1.0.0-dev";
+#include <pgmspace.h> // PROGMEM support header
+#include "Arduino.h"
+#include <SPI.h>
+// 
+#include "SPIFFS.h"
+#ifdef SDDAT3
+#include "SD.h"
+#endif
+
+#include <TFT_eSPI.h> // The TFT_eSPI library
+#include <BleKeyboard.h>       // BleKeyboard is used to communicate over BLE
+#include "BLEDevice.h"         // Additional BLE functionaity
+#include "BLEUtils.h"          // Additional BLE functionaity
+#include "BLEBeacon.h"         // Additional BLE functionaity
+#include "esp_sleep.h"         // Additional BLE functionaity
+#include "esp_bt_main.h"       // Additional BLE functionaity
+#include "esp_bt_device.h"     // Additional BLE functionaity
+#include "globals.hpp"
+#include "ConfigHelper.h"
+#include "Storage.h"
+#include "System.h"
+#include "Console.h"
+
+
+#include "Menu.h"
+#include "cJSON.h"
+#include "ImageWrapper.h"
+using namespace FreeTouchDeck;
+// ----------------------------------------------
+// It is possible to define you own custom action
+// to perform various actions on the ESP32 itself
+// (e.g. send the device to sleep, measure temperature
+// and send the result out as keypresses, etc.) and assign them
+// to buttons!
+// See examples under UserActions.h
+//
+#include "UserActions.h"
+
+// Create instances of the structs
+Wificonfig wificonfig = {.ssid = NULL, .password = NULL, .wifimode = NULL, .hostname = NULL};
+
+#ifdef ACTIONS_IN_TASKS
+//TaskHandle_t xScreenTask = NULL;
+//void ScreenHandleTask(void *pvParameters);
+TaskHandle_t xActionTask = NULL;
+void ActionTask(void *pvParameters);
+#endif
+
+
+// Checking for BLE Keyboard version
+#ifndef BLE_KEYBOARD_VERSION
+#warning Old BLE Keyboard version detected. Please update.
+#define BLE_KEYBOARD_VERSION "Outdated"
+#endif
+
+#include "ConfigLoad.h"
+#include "DrawHelper.h"
+#include "ConfigHelper.h"
+#include "MenuNavigation.h"
+
+
+//-------------------------------- SETUP --------------------------------------------------------------
+
+void setup()
+{
+  // Use serial port
+  Serial.begin(115200);
+  PrintBasicMemInfo();
+  PrintMemInfo(__FUNCTION__, __LINE__);
+  SetGeneralConfigDefaults();
+  Serial.setDebugOutput(true);
+  LOC_LOGI(module, "Starting system.");
+  PrintMemInfo(__FUNCTION__, __LINE__);
+  // Init Touch first. We will use it to restart the system when displaying a hard error
+  InitSystem();
+  
+  PrintMemInfo(__FUNCTION__, __LINE__);
+  // ---------------- Printing version numbers -----------------------------------------------
+  LOC_LOGI(module, "BLE Keyboard version: %s", BLE_KEYBOARD_VERSION);
+  LOC_LOGI(module, "TFT_eSPI version: %s", TFT_ESPI_VERSION);
+  LOC_LOGI(module, "SPI Flash frequency: %s", CONFIG_ESPTOOLPY_FLASHFREQ);
+
+  // ---------------- Start the first keypad -------------
+
+  // Draw background
+  if (RunMode== SystemMode::STANDARD && !GetActiveScreen())
+  {
+    // only set active screen to home if no screen is already active
+    // for example don't active home if drawError was called
+    SetActiveScreen("home");
+  }
+  PrintMemInfo(__FUNCTION__, __LINE__);
+  HandleSleepConfig();
+  HandleBeepConfig();
+#ifdef ACTIONS_IN_TASKS
+  // xTaskCreate(ScreenHandleTask, "Screen", 1024 * 3, NULL, tskIDLE_PRIORITY + 8, &xScreenTask);
+  // PrintMemInfo(__FUNCTION__, __LINE__);
+  // LOC_LOGD(module, "Screen task created");
+//  xTaskCreate(ActionTask, "Action", 1024 * 4, NULL, tskIDLE_PRIORITY + 5, &xActionTask);
+  //PrintMemInfo(__FUNCTION__, __LINE__);
+//  LOC_LOGD(module, "Action task created");
+#endif
+PrintBasicMemInfo();
+  PrintMemInfo(__FUNCTION__, __LINE__);
+}
+
+//--------------------- LOOP ---------------------------------------------------------------------
+void loop(void)
+{
+
+  try
+  {
+    processSerial();
+  }  
+  catch (const std::exception &e)
+  {
+      std::cerr << e.what() << '\n';
+  }
+  PrintMemStats();
+  processSleep();
+  HandleScreen();
+#ifndef ACTIONS_IN_TASKS
+  HandleActions();
+#endif
+  HandleActions();
+
+  // screen debounce
+  if(QueueSize()==0)
+  {
+    delay(50);
+  }
+  else 
+  {
+    delay(10);
+  }
+}
+#ifdef ACTIONS_IN_TASKS
+// void ScreenHandleTask(void *pvParameters)
+// {
+
+//   // Load menu definitions
+
+//   for (;;)
+//   {
+//     HandleScreen();
+//     delay(50);
+//   }
+// }
+
+void ActionTask(void *pvParameters)
+{
+  for (;;)
+  {
+    HandleActions();
+    delay(50);
+  }
+}
+#endif
